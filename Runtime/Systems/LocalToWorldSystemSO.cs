206732e0dc9cc70dfab426cf39d85d4c60ac8a4c
heymeepo <h3ystudio@gmail.com>
2024-01-25 13:23:02 +0400

    Add LocalToWorldSystemSO

---
+++ Runtime/Systems/LocalToWorldSystemSO.cs
@@ -0,0 +1,29 @@
+﻿using Scellecs.Morpeh.Systems;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace Scellecs.Morpeh.Transforms
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Systems/LocalToWorldSystem")]
+    public sealed class LocalToWorldSystemSO : UpdateSystem
+    {
+        private LocalToWorldSystem system;
+
+        public override void OnAwake()
+        {
+            system = new LocalToWorldSystem() { World = World };
+            system.OnAwake();
+        }
+
+        public override void OnUpdate(float deltaTime) => system.OnUpdate(deltaTime);
+
+        public override void Dispose()
+        {
+            system.Dispose();
+            system = null;
+        }
+    }
+}
