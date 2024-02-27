2123f5c056ba08bb55ca8cb2c3e2113743a958c3
heymeepo <h3ystudio@gmail.com>
2024-02-27 14:09:04 +0400

    Remove AsNativeArray filter extension

--- Runtime/MorpehWorkaround/FilterExtensions.cs
+++ Runtime/MorpehWorkaround/FilterExtensions.cs
@@ -1,9 +1,4 @@
-﻿using Scellecs.Morpeh;
-using Scellecs.Morpeh.Collections;
-using System.Runtime.CompilerServices;
-using Unity.Collections;
-
-namespace Scellecs.Morpeh.Workaround
+﻿namespace Scellecs.Morpeh.Workaround
 {
     public static class FilterExtensions
     {
@@ -58,48 +53,6 @@ namespace Scellecs.Morpeh.Workaround
                 includeHash = builder.includeHash,
                 excludeHash = builder.excludeHash ^ typeId
             };
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static NativeArray<int> AsNativeArray(this Filter filter)
-        {
-            int totalEntities = 0;
-
-            for (int i = 0, len = filter.archetypes.length; i < len; i++)
-            {
-                totalEntities += filter.archetypes.data[i].entities.count;
-            }
-
-            var nativeArray = new NativeArray<int>(totalEntities, Allocator.TempJob);
-            int position = 0;
-
-            // Проходим по каждому архетипу и каждому чанку в архетипе
-            for (int i = 0, len = filter.archetypes.length; i < len; i++)
-            {
-                var archetype = filter.archetypes.data[i];
-                // Предполагаем, что есть способ получить доступ к сущностям в архетипе напрямую
-                foreach (var entityId in archetype.entities)
-                { // Перебор должен соответствовать реальной структуре данных
-
-                    nativeArray[position++] = entityId;
-                }
-            }
-
-            return nativeArray;
-        }
-
-        public static BitMap[] GetArchetypes(this Filter filter, out int length)
-        {
-            var array = new BitMap[filter.archetypes.length];
-            length = 0;
-
-            for (int i = 0; i < filter.archetypes.length; i++)
-            {
-                array[i] = filter.archetypes.data[i].entities;
-                length += array[i].count;
-            }
-
-            return array;
         }
     }
 }
